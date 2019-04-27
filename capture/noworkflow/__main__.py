c18da6671ba6808dfc16b10dabb1ae538b24af68
rmparanhos <raffaelmp@id.uff.br>
2019-04-26 23:29:26 -0300

    initial commit

--- capture/noworkflow/__main__.py
+++ capture/noworkflow/__main__.py
@@ -3,6 +3,8 @@
 # This file is part of noWorkflow.
 # Please, consult the license terms in the LICENSE file.
 
+# Teste commit
+
 from __future__ import (absolute_import, print_function,
                         division, unicode_literals)
 
